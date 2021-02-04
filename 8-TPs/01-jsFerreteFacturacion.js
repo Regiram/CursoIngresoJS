e5401beacbc2f4fb50da12d88c9377d92a25195f
Facundo Gonzalez <facundog.4264@gmail.com>
2021-02-04 18:46:32 -0300

    cambios

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,31 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    var precio1, precio2, precio3, resultado;
+    precio1 = document.getElementById("txtIdPrecioUno").value;
+    precio2 = document.getElementById("txtIdPrecioDos").value;
+    precio3 = document.getElementById("txtIdPrecioTres").value;
+
+    resultado = parseFloat(precio1) + parseFloat(precio2) + parseFloat(precio3);
+	alert ("La suma de los numeros ingresados es de: " + resultado);
 }
 function Promedio () 
 {
-	
+	var precio1, precio2, precio3, resultado;
+    precio1 = document.getElementById("txtIdPrecioUno").value;
+    precio2 = document.getElementById("txtIdPrecioDos").value;
+    precio3 = document.getElementById("txtIdPrecioTres").value;
+
+    resultado = (parseFloat(precio1) + parseFloat(precio2) + parseFloat(precio3)) / 3;
+    alert ("El promedio de los numeros ingresados es de: " + resultado);
 }
 function PrecioFinal () 
 {
-	
+	var precio1, precio2, precio3, resultado;
+    precio1 = document.getElementById("txtIdPrecioUno").value;
+    precio2 = document.getElementById("txtIdPrecioDos").value;
+    precio3 = document.getElementById("txtIdPrecioTres").value;
+
+    resultado = (parseFloat(precio1) + parseFloat(precio2) + parseFloat(precio3)) * 1.21;
+    alert ("La suma de los precios incluyendo el IVA es de: " + resultado);
 }
