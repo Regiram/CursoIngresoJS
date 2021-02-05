a6a70205eec1cd4f850472c555af00dcb62c782d
Facundo Gonzalez <facundog.4264@gmail.com>
2021-02-04 22:21:59 -0300

    cambios

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -2,17 +2,36 @@
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
 C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
+
+FACUNDO GONZALEZ
+
 */
 
 function Rectangulo () 
 {
+    var largo, ancho, alambre;
+    largo = document.getElementById("txtIdLargo").value;
+    ancho = document.getElementById("txtIdAncho").value;
 
+    alambre = ((parseFloat(largo) * 2) + (parseFloat(ancho) * 2)) * 3;
+    alert ("Debe comprar " + alambre + "MTRS  de alambre para cubrir 3 veces el terreno.");
 }
 function Circulo () 
 {
+    var radio, alambre;
+    radio = document.getElementById("txtIdRadio").value;
+
+    alambre = (2 * Math.PI  * parseFloat(radio)) * 3;
+    alert ("Debe comprar " + alambre + " MTRS de alambre para cubrir 3 veces el terreno.");
 	
 }
 function Materiales () 
 {
-	
+	var largo, ancho, cal, cemento;
+    largo = document.getElementById("txtIdLargo").value;
+    ancho = document.getElementById("txtIdAncho").value;
+
+    cemento = (parseFloat(largo) * parseFloat(ancho))*2;
+    cal = (parseFloat(largo) * parseFloat(ancho))*3;
+    alert ("Debe comprar " + cemento + " bolsas de Cemento y " + cal + " bolsas de Cal acorde al area del terreno ingresado.");
 }
--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,13 +1,25 @@
 /*3.	Para el departamento de Pinturas:
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
+
+FACUNDO GONZALEZ
+
 */
 function FahrenheitCentigrados () 
-{
+{  
+    var temperatura, fahrAcent;
+    temperatura = document.getElementById("txtIdTemperatura").value;
+
+    fahrAcent= parseFloat(temperatura - 32) * (5/9);
+    alert (temperatura + "° Fahrenheit son " + fahrAcent + "° Centígrados");
 	
 }
 
 function CentigradosFahrenheit () 
 {
-	
+	var temperatura, centAfahr;
+    temperatura = document.getElementById("txtIdTemperatura").value;
+
+    centAfahr= parseFloat(temperatura * (9/5)) + 32;
+    alert (temperatura + "° Centígrados son " + centAfahr + "° Fahrenheit");
 }
