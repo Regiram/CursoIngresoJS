34a2e060df8f360e32daa471036546d023d1a43a
Facundo Gonzalez <facundog.4264@gmail.com>
2021-02-09 22:31:13 -0300

    cambios

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -7,8 +7,32 @@ D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es de
 E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
+    FACUNDO GONZALEZ
+
  */
 function CalcularPrecio () 
 {
- 	
+ 	var cantidadLamparas, marcaLampara, costoLampara, precio, precioFinal, descuento, operacionOFF;
+
+    cantidadLamparas = document.getElementById("txtIdCantidad").value;
+    marcaLampara = document.getElementById("Marca").value;
+
+    cantidadLamparas = parseInt(cantidadLamparas);
+    costoLampara = parseInt(35);
+    
+    precio = parseInt(cantidadLamparas * costoLampara);
+    
+    if (cantidadLamparas > 5){
+        descuento = 0.5;
+    }
+    else{
+        if (marcaLampara == "ArgentinaLuz" && cantidadLamparas == 5) {
+            descuento = 0.6;
+        }
+        else {
+            descuento = 0.7;
+        }
+    }
+    operacionOFF = parseInt(precio * descuento);
+    precioFinal = document.getElementById("txtIdprecioDescuento").value = operacionOFF;
 }
