aea464e09c0decb3ab635ceb4f19661d1ea12149
Facundo Gonzalez <facundog.4264@gmail.com>
2021-02-22 22:01:38 -0300

    nuevos cambios

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -8,13 +8,19 @@ FACUNDO GONZALEZ
 function mostrar()
 {	
 	var banderaPrimero;
+	var banderaNumeroPar;
+	var banderaNumeroNegativo;
 	var numeroIngresado;
 	var numeroMaximo;
 	var numeroMinimo;
 	var pregunta;
+	var menorPar;
+	var mayorNegativo;
 
 	banderaPrimero="primero";
-	pregunta='si';
+	pregunta ='si';
+	banderaNumeroPar = "par"
+	banderaNumeroNegativo = "negativo"
 
 	while (pregunta == "si"){	
 		numeroIngresado = prompt ("Ingrese un numero");
@@ -27,8 +33,17 @@ function mostrar()
 			numeroMinimo = numeroIngresado;
 			banderaPrimero = "no";
 		}
+		if ((banderaNumeroPar == "par" && numeroIngresado % 2 == 0)  || numeroIngresado < menorPar){
+			menorPar = numeroIngresado;
+			banderaNumeroPar = "impar";
+		}
+		if ((banderaNumeroNegativo == "negativo" && numeroIngresado < 0) || (numeroIngresado > mayorNegativo && numeroIngresado < 0)){
+			mayorNegativo = numeroIngresado;
+			banderaNumeroNegativo = "positivo";
+		}
 		pregunta=prompt("desea continuar? (si/no)");
 	}
 	txtIdMaximo.value=numeroMaximo;
 	txtIdMinimo.value=numeroMinimo;
+	alert ("De los numeros ingresados el menor dentro de los pares es: " + menorPar +" y el mayor dentro de nuemeros negativos " + mayorNegativo);
 }
